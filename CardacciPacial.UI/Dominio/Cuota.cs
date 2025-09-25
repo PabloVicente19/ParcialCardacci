4664c3b1ec0526b8d951aa674605ce083aa91d16
Pablo Vicente <pablovicente19@hotmail.com>
2025-09-25 15:10:48 -0300

    Agregar archivos de proyecto.

---
+++ CardacciPacial.UI/Dominio/Cuota.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CardacciPacial.UI.Clases;
+
+namespace CardacciPacial.UI.Dominio
+{
+    public class Cuota
+    {
+        public int CuotaId { get; set; }
+        public DateTime FechaPago { get; set; }
+        public Cliente Cliente { get; set; }
+        public Membresia Membresia { get; set; }
+
+    }
+}
---
+++ CardacciPacial.UI/Dominio/Membresia.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardacciPacial.UI.Dominio
+{
+    public class Membresia
+    {
+        public string MembresiaId { get; set; }
+        public DateTime FechaInicio { get; set; }
+        public double Valor { get; set; }
+        public byte Descuento { get; private set; }
+    }
+}
